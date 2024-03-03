e5e9695eda363ca838b6546c38f2eadbdf8e46e1
Meghana <Meghana@DESKTOP-S54S9MI>
2024-03-03 11:39:35 +0530

    Date classes added

---
+++ JavaPractice/src/com/practice/DateTest.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+import java.util.Date;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		Date d = new Date();
+		System.out.println(d.toString());
+	}
+
+}
