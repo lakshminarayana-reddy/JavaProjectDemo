040a7e311610924bbb9cb8bb52fa79df1dd1727c
LakshmiNarayana <klnr2205@gmail.com>
2024-03-23 14:35:33 +0530

    Added demo class in string programming branch

---
+++ JavaPractice/src/com/mapinterface/Demo.java
@@ -0,0 +1,10 @@
+package com.mapinterface;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Making conflicts wantedly");
+	}
+
+}
