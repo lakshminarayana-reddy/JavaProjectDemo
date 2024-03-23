8c34015c034a0b5aadc983d761b29cc25a386eea
LakshmiNarayana <klnr2205@gmail.com>
2024-03-23 11:32:04 +0530

    Pushing to main

--- JavaPractice/src/com/practice/TwoDArray.java
+++ JavaPractice/src/com/practice/TwoDArray.java
@@ -1,22 +1,28 @@
 package com.practice;
 
+import java.util.Hashtable;
+
 public class TwoDArray {
+	static Hashtable<String, String> table=null;
 
 	public static void main(String[] args) {
 		storeData();
+		Hashtable<String,String> data2=table;
+		System.out.println(data2.get("1A"));
 	}
 	public static Object[][] storeData(){
-		Object data[][] = new Object[3][4];
-		for(int i=0;i<2;i++) {
-			for(int j=0;j<2;j++) {
-				data[i][j]=i+j;
-				System.out.print(i+"**"+j+"="+data[i][j]);
-				System.out.print("  ");
+		Object data[][] = new Object[2][2];
+		
+		for(int i=0; i<2; i++) {
+			table=new Hashtable<String,String>();
+			for(int j=0; j<2; j++) {
+				table.put(String.valueOf(i)+"A",String.valueOf(j));
+				System.out.println(table);
+				data[i][j]=table;
+				//System.out.println(data);
 			}
-			System.out.println();
 		}
-		System.out.println(data[1][4]);
+		//System.out.println(data);
 		return data;
 	}
-
 }
