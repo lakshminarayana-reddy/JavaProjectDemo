f8a85d647d975009b2cd8c53f3e760e66001b5ee
LakshmiNarayana <klnr2205@gmail.com>
2024-03-23 14:25:23 +0530

    Testing github actions

--- JavaPractice/src/com/collections/Test.java
+++ JavaPractice/src/com/collections/Test.java
@@ -5,5 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
 	}
+	public static void testGit() {
+		System.out.println("Test git hub actions");
+	}
 
 }
